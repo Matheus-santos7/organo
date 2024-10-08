0adb1b8618c231585a41bd85d6c117c696ec51ec
Matheus-santos7 <matheussantosgc@icloud.com>
2024-10-08 20:31:26 -0300

    Feat:

---
+++ src/components/Banner/index.js
@@ -0,0 +1,11 @@
+import './Banner.css';
+
+const Banner = () => {
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="Imagem do banner organo" />
+        </header>
+    )
+}
+
+export default Banner;
